c85001b8c2e81b9716095d25b0efb64f606af6ed
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-04-27 15:57:24 +0900

    fix header design

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,35 @@
+'use client';
+
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 import NavLink from '@/components/header/nav-link';
 import ThemeToggle from '@/components/theme-toggle';
 
 const NAV_ITEMS = [
-  { path: 'blog', name: '작업들' },
+  { path: 'blog', name: '작업' },
   { path: 'about', name: '회사소개' },
-  { path: 'projects', name: '공사 실적' },
-  { path: 'contact', name: '상담 및 견적문의' },
+  { path: 'projects', name: '공사실적' },
+  { path: 'contact', name: '문의' },
 ];
 
 export default function Header() {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const user = navigator.userAgent;
+    if (user.indexOf('iPhone') > -1 || user.indexOf('Android') > -1) {
+      setMobile(true);
+    }
+  }, [mobile]);
+
   return (
     <nav className="flex flex-col justify-between px-8 py-12 md:flex-row">
-      <Link href="/" className="self-start md:self-auto">
+      <Link href="/blog" className="self-start md:self-auto">
         <h1 className="text-3xl font-bold">(주) CM3네트워크</h1>
       </Link>
       <div className="my-6 flex space-x-8 self-center md:my-0 md:self-auto">
-        <ul className="flex space-x-8">
+        <ul className="flex space-x-8 ">
           {NAV_ITEMS.map((item) => (
             <li
               key={item.path}
@@ -27,9 +39,11 @@ export default function Header() {
             </li>
           ))}
         </ul>
-        <div className="absolute right-[10vw] top-12 md:static">
-          <ThemeToggle />
-        </div>
+        {!mobile && (
+          <div className="absolute right-[10vw] top-12 md:static">
+            <ThemeToggle />
+          </div>
+        )}
       </div>
     </nav>
   );
